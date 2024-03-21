daefc3f4b776754e25b42ac1297f7e647f97fbab
Dr0med4r <trompeter-stoll@gmx.de>
2024-03-21 22:44:29 +0100

    remove print and rename function

--- lua/autosemicolon/nix.lua
+++ lua/autosemicolon/nix.lua
@@ -13,13 +13,10 @@ M.add_semicolon = function(args)
     local tree = parser:parse()[1]
     local root = tree:root()
     local lang = parser:lang()
-    print("hi")
     local query = ts.query.parse(lang, query_string)
     for _, matches, _ in query:iter_matches(root, args.buf, 0, -1) do
         for _, match in pairs(matches) do
             local row, column, _ = match:end_()
-            print(row)
-            print(column)
             vim.api.nvim_buf_set_text(args.buf, row, column, row, column, { ";" })
         end
     end
@@ -27,10 +24,11 @@ end
 
 
 
-M.aucmd = function()
+M.autocmd = function()
     vim.api.nvim_create_autocmd(
         { "InsertLeave" },
         {
+            group = vim.api.nvim_create_augroup("Autosemicolon", {}),
             pattern = { "*.nix" },
             callback = M.add_semicolon,
         }
