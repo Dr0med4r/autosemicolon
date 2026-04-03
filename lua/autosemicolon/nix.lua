c800ad5aca4f488aa4c80a213e424c3eafaf85e6
Dr0med4r <trompeter-stoll@gmx.de>
2026-04-03 14:32:59 +0200

    improve treesitter query to find more problems

--- lua/autosemicolon/nix.lua
+++ lua/autosemicolon/nix.lua
@@ -7,6 +7,13 @@ local query_string = [[
     function:(_) @wrong
     argument:(_))
 (ERROR))
+
+((_)
+ (ERROR
+   (apply_expression
+     function: (_)@wrong)
+   )
+ expression:(_))
 ]]
 
 M.add_semicolon = function(args)
