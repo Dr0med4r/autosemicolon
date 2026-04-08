6f8f0b7d910f5a4d1f4d5142a7175fd046fd4b66
dr0med4r <trompeter-stoll@gmx.de>
2026-04-08 21:05:49 +0200

    define the rules more precisely

--- lua/autosemicolon/nix.lua
+++ lua/autosemicolon/nix.lua
@@ -4,10 +4,20 @@ local ts = vim.treesitter
 
 local query_string = [[
 ((_
-    function:(_) @wrong
-    argument:(_))
-(ERROR))
+    function:(_) @function
+    argument:(_) @argument)
+(ERROR) @error)
 ]]
+
+local function add_semicolon_after_node(buffer, node)
+    local _, _, end_row, end_col = node:range()
+    vim.api.nvim_buf_set_text(buffer, end_row, end_col, end_row, end_col, { ";" })
+end
+
+local function get_node_content(buffer, node)
+    local start_row, start_col, end_row, end_col = node:range()
+    return vim.api.nvim_buf_get_text(buffer, start_row, start_col, end_row, end_col, {})
+end
 
 M.add_semicolon = function(args)
     local parser = assert(ts.get_parser(args.buf))
@@ -16,14 +26,21 @@ M.add_semicolon = function(args)
     local lang = parser:lang()
     local query = ts.query.parse(lang, query_string)
     for _, match, _ in query:iter_matches(root, args.buf) do
-        for _, nodes in pairs(match) do
-            for _, node in pairs(nodes) do
-                local row, column, _ = node:end_()
-                vim.api.nvim_buf_set_text(args.buf, row, column, row, column, { ";" })
-            end
+        -- assume the captures are always in the same order
+        local function_node = match[1][1]
+        local argument_node = match[2][1]
+        local error_node = match[3][1]
+
+        local error_node_content = get_node_content(args.buf, error_node)
+        if error_node_content[1] == "=" then
+            add_semicolon_after_node(args.buf, function_node)
+        end
+        if error_node_content[1] == "}" then
+            add_semicolon_after_node(args.buf, argument_node)
         end
     end
 end
+
 
 
 
